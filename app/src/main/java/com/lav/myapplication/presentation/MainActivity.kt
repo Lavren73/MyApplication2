67286e86032cc1c7f6bbdd10e1df74bfa6112340
Lavrenov <lavrenovau@gmail.com>
2023-04-10 00:27:15 +0300

    Creating project

--- app/src/main/java/com/lav/myapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/lav/myapplication/presentation/MainActivity.kt
@@ -2,19 +2,56 @@ package com.lav.myapplication.presentation
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.lav.myapplication.R
+import com.lav.myapplication.domain.ShopItem
+import com.lav.myapplication.domain.ShopListRepository
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var llShopList: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        llShopList = findViewById(R.id.ll_shop_list)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
-            Log.d("MainActivityTest", it.toString())
+            showList(it)
+//            Log.d("MainActivityTest", it.toString())
         }
     }
+
+
+    private fun showList(list: List<ShopItem>) {
+
+        llShopList.removeAllViews()
+
+        for (shopItem in list) {
+            val layOutId = if (shopItem.enabled) {
+                R.layout.item_shop_enabled
+            } else {
+                R.layout.item_shop_disabled
+            }
+            val view = LayoutInflater.from(this).inflate(layOutId, llShopList, false)
+            val tvName = view.findViewById<TextView>(R.id.tv_name)
+            val tvCount = view.findViewById<TextView>(R.id.tv_count)
+
+            tvName.text = shopItem.name
+            tvCount.text = shopItem.count.toString()
+            view.setOnClickListener{
+                viewModel.changeEnableState(shopItem)
+                true
+            }
+
+            llShopList.addView(view)
+        }
+    }
+
 }
+
